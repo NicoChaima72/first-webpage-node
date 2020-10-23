7a5106fb1ca8db93f6537adf8e47fdabcb2dd992
Nicolas Chaima <nikochaima72@gmail.com>
2020-10-23 03:45:14 -0300

    Add route about

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,10 @@ router.get("/", (req, res) => {
 	res.render("index.html", { title: "First website", file: "home" });
 });
 
+router.get("/about", (req, res) => {
+	res.render("about.html", { title: "About website", file: "about" });
+});
+
 router.get("/contact", (req, res) => {
 	res.render("contact.html", { title: "Contact website", file: "contact" });
 });
